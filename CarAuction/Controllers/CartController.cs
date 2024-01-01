25656b3e2896138bf259a129b5b8578c0f9637bd
Cарофанов F <billyroll03@gmail.com>
2024-01-02 02:15:02 +0300

    add summary

--- CarAuction/Controllers/CartController.cs
+++ CarAuction/Controllers/CartController.cs
@@ -1,13 +1,20 @@
 ﻿using CarAuction.Data;
 using CarAuction.Models;
+using CarAuction.Models.ViewModels;
 using CarAuction.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CarAuction.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly AppDbContext _db;
+
+        [BindProperty]
+        public VehicleUserVM VehicleUserVM { get; set; }
 
         public CartController(AppDbContext db)
         {
@@ -29,6 +36,55 @@ namespace CarAuction.Controllers
             List<Vehicle> vehicles = _db.Vehicles.Where(u => vehicleInCart.Contains(u.Id)).ToList();
 
             return View(vehicles);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var shoppingCarts = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count > 0)
+            {
+                //session exsits
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCarts.Remove(shoppingCarts.FirstOrDefault(u=>u.VehicleId == id));
+
+            HttpContext.Session.Set(WC.SessionCart, shoppingCarts);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var shoppingCarts = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count > 0)
+            {
+                //session exsits
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> vehicleInCart = shoppingCarts.Select(i => i.VehicleId).ToList();
+            List<Vehicle> vehicles = _db.Vehicles.Where(u => vehicleInCart.Contains(u.Id)).ToList();
+
+            VehicleUserVM = new VehicleUserVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == claim.Value),
+                Vehicles = vehicles
+            };
+
+            return View(VehicleUserVM);
         }
     }
 }
---
+++ CarAuction/Models/ViewModels/VehicleUserVM.cs
@@ -0,0 +1,13 @@
+﻿namespace CarAuction.Models.ViewModels
+{
+    public class VehicleUserVM
+    {
+        public VehicleUserVM()
+        {
+            Vehicles = new List<Vehicle>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Vehicle> Vehicles { get; set; }
+    }
+}
